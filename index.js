8ac6927d764a107058e5a2226032614397aff814
Lalon Sheikh <you@example.com>
2024-08-28 07:33:23 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+    ],
+    credentials: true,
+    optionSuccessStatus: 200,
+  }
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -27,13 +36,13 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
